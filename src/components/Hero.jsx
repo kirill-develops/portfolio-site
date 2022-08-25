d121c958739db03d239970766985df4ab2d2f6e9
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-24 23:01:16 -0400

    Styled Hero banner & added contact link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,39 @@ import { Section, Title, TitleAccent, TitleLink } from '../styles/globalStyles';
 
 const HeroSection = styled(Section)`
   height: 100vh;
+  transform: translateY(15%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (min-width: 37.5rem) {
+    transform: translateY(-10%);
+    width: 75%;
+    margin: auto;
+  }
+`;
+
+const HeroTitle = styled(Title)`
+  max-width: 220px;
+`;
+
+const HeroAccent = styled(TitleAccent)`
+  color: #1c52a2;
 `;
 
 const Hero = () => {
   return (
     <HeroSection id='top'>
-      <Title>
+      <HeroTitle>
         Kirill Tchentsov
         <br />
-        <TitleAccent>Web Developer</TitleAccent>
-      </Title>
+        <HeroAccent>Web Developer</HeroAccent>
+      </HeroTitle>
       <TitleLink href='#about'>Learn More</TitleLink>
       <br />
       <TitleLink href='#projects'>Projects</TitleLink>
+      <br />
+      <TitleLink href='#contact'>Contact Me</TitleLink>
     </HeroSection>
   );
 };
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -26,9 +26,9 @@ export const Main = styled.main`
 export const Section = styled.section`
   min-height: fit-content;
   padding: 12px 16px 96px;
+  height: 100vh;
 
   @media (min-width: 37.5rem) {
-    height: 100vh;
     padding: 32px 32px 96px;
   }
 `;
