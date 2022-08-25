9a73fba345aa51db17e3d65ff8406c85da51a891
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-25 11:08:28 -0400

    added divider to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SiGmail, SiGithub, SiLinkedin, SiClickup } from 'react-icons/si';
-import { Section, Title } from '../styles/globalStyles';
+import { Section, Title, Body } from '../styles/globalStyles';
 
 const Contact = styled(Section)`
   height: inherit;
   padding-bottom: 64px;
-  background-color: #0a0908;
+  background-color: #272727;
   color: #efecef;
 `;
 
-const LinksWrapper = styled.address`
+const LinksContainer = styled.address`
   display: flex;
   margin: 0 auto;
   justify-content: space-between;
@@ -26,36 +26,51 @@ const LogoWrapper = styled.a`
 
 const GithubWrapper = styled(LogoWrapper)`
   &: hover {
-    color: #207729;
+    color: #00ac47;
   }
 `;
 
 const GmailWrapper = styled(LogoWrapper)`
   &: hover {
-    color: #be2624;
+    color: #ea4335;
   }
 `;
 
 const LinkedinWrapper = styled(LogoWrapper)`
   &: hover {
-    color: #0e4fb4;
+    color: #0a66c2;
   }
+`;
+
+const Divider = styled.hr`
+  width: 50%;
+  min-width: 300px;
+  border-color: #6d6d6d;
 `;
 
 const ClickUpWrapper = styled(LogoWrapper)`
   display: flex;
   width: fit-content;
-  margin: 2.4rem auto 0;
+  margin: 0 auto 2.4rem;
   &: hover {
     color: #d58a27;
   }
+`;
+
+const Caption = styled(Body)`
+  font-size: 0.8rem;
+  text-align: center;
+  color: #6d6d6d;
 `;
 
 function Footer() {
   return (
     <Contact id='contact'>
       <Title> Connect </Title>
-      <LinksWrapper>
+      <ClickUpWrapper href='#top'>
+        <SiClickup size={22} />
+      </ClickUpWrapper>
+      <LinksContainer>
         <LinkedinWrapper
           href='https://www.linkedin.com/in/kirill-tchentsov/'
           target='_blank'
@@ -73,10 +88,9 @@ function Footer() {
         >
           <SiGithub size={28} />
         </GithubWrapper>
-      </LinksWrapper>
-      <ClickUpWrapper href='#top'>
-        <SiClickup size={22} />
-      </ClickUpWrapper>
+      </LinksContainer>
+      <Divider />
+      <Caption>Built with Gatsby and React</Caption>
     </Contact>
   );
 }
