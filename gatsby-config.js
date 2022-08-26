c0a740d95d1facd8563144726c2ecb0ebbce1376
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-25 22:27:33 -0400

    added swiper element for mobile Project component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Kirill Tchentsov | Web Developer`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'Kirill Tchentsov | Web Developer',
+    siteUrl: 'https://www.yourdomain.tld',
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "images"
-  }]
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-json',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: './src/data/',
+      },
+    },
+  ],
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Title, Section } from '../styles/globalStyles';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Parallax, Autoplay } from 'swiper';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { Title, Section, Body } from '../styles/globalStyles';
 
-const Projects = () => {
+import '../styles/swiper.css';
+// Swiper style dependencies
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/pagination';
+import 'swiper/css/parallax';
+
+function Projects() {
+  const data = useStaticQuery(graphql`
+    query {
+      allProjectListJson {
+        nodes {
+          id
+          name
+          description
+          img {
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: TRACED_SVG
+                layout: CONSTRAINED
+                width: 500
+              )
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const projectSlides = data.allProjectListJson?.nodes?.map((project) => {
+    const projectPhoto = getImage(project.img);
+
+    return (
+      <SwiperSlide key={project.id}>
+        <GatsbyImage
+          image={projectPhoto}
+          alt={`Screenshot of ${project.name}`}
+        />
+        <h3>{project.name}</h3>
+        <Body>{project.description}</Body>
+      </SwiperSlide>
+    );
+  });
+
   return (
-    <Section id='projects'>
+    <Section id="projects">
       <Title>Projects</Title>
+      <Swiper
+        modules={[Pagination, Parallax, Autoplay]}
+        pagination={{ type: 'bullets' }}
+        parallax
+        // autoplay
+      >
+        {projectSlides}
+      </Swiper>
     </Section>
   );
-};
+}
 
 export default Projects;
