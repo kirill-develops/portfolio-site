f8ae1d678ffa52cce489f0123889d870a66f0c97
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-26 03:00:35 -0400

    added photo style & changed body content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 import { Title, Body, BodyAccent, Section } from '../styles/globalStyles';
 
 const AboutSection = styled(Section)`
-  background-color: #1c52a2;
-  color: #efecef;
+  background-color: #2f2756;
+  color: #ebdbe2;
   display: flex;
   flex-direction: column;
   height: inherit;
+
   @media (min-width: 37.5rem) {
     height: 100vh;
   }
@@ -22,7 +23,7 @@ const FlexWrapper = styled.div`
   flex-basis: min-content;
   justify-content: center;
   align-items: center;
-  gap: 0 24px;
+  gap: 12px 24px;
 
   @media (min-width: 70rem) {
     gap: 0 32px;
@@ -30,13 +31,16 @@ const FlexWrapper = styled.div`
 `;
 
 const PhotoWrapper = styled.div`
-  border-radius: 3px;
-  box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important;
+  border-radius: 6px;
+  border: 2px solid #c2b368;
+  box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.375);
   overflow: hidden;
+  margin: 0 16px;
   max-width: 25rem;
 
   @media (min-width: 37.5rem) {
     width: 40%;
+    margin: 0;
     max-width: fit-content;
   }
 `;
@@ -44,8 +48,11 @@ const PhotoWrapper = styled.div`
 const BodyWrapper = styled.article`
   min-width: 280px;
   width: 100%;
+  padding: 0 8px;
+  text-align: center;
 
   @media (min-width: 37.5rem) {
+    text-align: start;
     width: 40%;
   }
 `;
@@ -53,17 +60,17 @@ const BodyWrapper = styled.article`
 function About() {
   const ScubaPhoto = (
     <StaticImage
-      src='../assets/Scuba.jpg'
-      alt='Kirill in Scuba Equipment'
-      placeholder='tracedSVG'
-      layout='constrained'
+      src="../images/Scuba.jpg"
+      alt="Kirill in Scuba Equipment"
+      placeholder="tracedSVG"
+      layout="constrained"
     />
   );
 
   return (
     <AboutSection
-      id='about'
-      color='purple'
+      id="about"
+      color="purple"
     >
       <Title>About Me</Title>
       <FlexWrapper>
@@ -76,32 +83,32 @@ function About() {
           </Body>
           <Body>
             I first fell in love with programming in grade 7, creating
-            side-scrolling QBasic games. More recently I've been creating
-            Full-Stack projects built on modern frameworks like
-            <BodyAccent> React.js, Node.js, MySQL </BodyAccent>
-            and
-            <BodyAccent> JavaScript</BodyAccent>.
+            side-scrolling QBasic games way before Pokemon was a thing. More
+            recently I've been creating Full-Stack projects built on modern
+            frameworks like
+            <BodyAccent> React.js, Node.js, MySQL </BodyAccent>&
+            <BodyAccent> JavaScript</BodyAccent> and I'm always eager to learn
+            new languages & frameworks.
           </Body>
           <Body>
             My love for programming stems from a need to be creative and the
-            satisfaction you gain from solving problems. I'm always eager to
-            learn new languages and frameworks, and excited about all the
-            possibilites this industry provides.
+            satisfaction you gain from solving problems. I'm excited about all
+            the possibilites this industry provides.
           </Body>
           <Body>
             If you would like to talk more, I can be contacted below via email
             or LinkedIn. Thanks for dropping by!
           </Body>
           <Body>
-            <BodyAccent>FrontEnd: </BodyAccent>
+            <BodyAccent color="#EFEFEF">FrontEnd: </BodyAccent>
             React.js, Redux, HTML5, CSS3, AJAX, Next.js
           </Body>
           <Body>
-            <BodyAccent>BackEnd: </BodyAccent>
+            <BodyAccent color="#EFEFEF">BackEnd: </BodyAccent>
             Python, Node.js, Express.js, Passport.js
           </Body>
           <Body>
-            <BodyAccent>Database: </BodyAccent>
+            <BodyAccent color="#EFEFEF">Database: </BodyAccent>
             MySQL, Knex.js, MongoDB
           </Body>
         </BodyWrapper>
