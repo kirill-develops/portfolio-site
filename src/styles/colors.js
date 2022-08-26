1a0b471bd6dd4ddb5015e382fcff33d74f45d9d9
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-26 14:19:17 -0400

    created global color & media query objects

---
+++ src/styles/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+  darkShade: '#2f2756',
+  darkAccent: '#7D747D',
+  mainColor: '#4795cf',
+  lightAccent: '#c2b368',
+  lightShade: '#ebdbe2',
+  white: '#fcf2ee',
+  black: '#272727',
+  logoInactive: '#c9cbcd',
+  logoGithub: '#00ac47',
+  logoGmail: '#ea4335',
+  logoLinkedin: '#0a66c2',
+  logoUp: '#d58a27',
+  finePrint: '#6d6d6d',
+};
+
+export default colors;
---
+++ src/styles/mediaQueries.js
@@ -0,0 +1,31 @@
+import { css } from 'styled-components';
+
+export const mobileBreakpoint = 481 / 16;
+export const tabletBreakpoint = 600 / 16;
+export const laptopBreakpoint = 1024 / 16;
+export const desktopBreakpoint = 1080 / 16;
+
+const media = {
+  devicePortrait: (...args) => css`
+    @media (min-width: ${tabletBreakpoint}em) and (max-width: ${laptopBreakpoint}em) and (orientation: portrait) {
+      ${css(...args)}
+    }
+  `,
+  deviceLandscape: (...args) => css`
+    @media (min-width: ${mobileBreakpoint}em) and (max-width: ${laptopBreakpoint}em) and (orientation: landscape) {
+      ${css(...args)}
+    }
+  `,
+  laptop: (...args) => css`
+    @media (min-width: ${laptopBreakpoint}em) and (max-width: ${desktopBreakpoint}em) {
+      ${css(...args)}
+    } ;
+  `,
+  desktop: (...args) => css`
+    @media (min-width: ${desktopBreakpoint}em) {
+      ${css(...args)}
+    } ;
+  `,
+};
+
+export default media;
