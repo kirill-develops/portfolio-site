88fe81294d6f01d10c56f298ae9b86d87bcf0b6d
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-26 16:06:21 -0400

    added landscape media query expression to media obj

--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -9,22 +9,27 @@ const media = {
   devicePortrait: (...args) => css`
     @media (min-width: ${tabletBreakpoint}em) and (max-width: ${laptopBreakpoint}em) and (orientation: portrait) {
       ${css(...args)}
-    }
+    };
   `,
   deviceLandscape: (...args) => css`
     @media (min-width: ${mobileBreakpoint}em) and (max-width: ${laptopBreakpoint}em) and (orientation: landscape) {
       ${css(...args)}
-    }
+    };
   `,
   laptop: (...args) => css`
     @media (min-width: ${laptopBreakpoint}em) and (max-width: ${desktopBreakpoint}em) {
       ${css(...args)}
-    } ;
+    };
   `,
   desktop: (...args) => css`
     @media (min-width: ${desktopBreakpoint}em) {
       ${css(...args)}
-    } ;
+    };
+  `,
+  landscape: (...args) => css`
+    @media (orientation: landscape) {
+      ${css(...args)}
+    };
   `,
 };
 
