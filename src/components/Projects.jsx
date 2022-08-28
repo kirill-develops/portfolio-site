0b2b5ce4f1c19f932f2b0e8fe4ec0d41d4026c6b
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-28 13:32:35 -0400

    created custom hook to determine how Projects will render

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,180 +1,93 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay, Mousewheel } from 'swiper';
-import { graphql, useStaticQuery } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
-import { Title, Section, Body } from '../styles/globalStyles';
+import { Swiper } from 'swiper/react';
+import { Pagination, Autoplay, Mousewheel, EffectCube } from 'swiper';
+import { Parallax } from 'react-scroll-parallax';
+import useProjectSlide from '../utils/useProjectSlides';
+import useMediaQuery from '../utils/useMediaQuery';
+import { Title, Section } from '../styles/globalStyles';
+import { laptopBreakpoint, mobileBreakpoint } from '../styles/mediaQueries';
 import colors from '../styles/colors';
+import ProjectLaptopView from './ProjectLaptopView';
 
 // Swiper style dependencies
 import 'swiper/scss';
+import 'swiper/scss/effect-cube';
+import 'swiper/scss/mousewheel';
 import 'swiper/scss/autoplay';
 import 'swiper/scss/pagination';
 import '../styles/swiper.scss';
-import media from '../styles/mediaQueries';
-import { Parallax } from 'react-scroll-parallax';
 
+// styled-component for Projects section
 const ProjectsSection = styled(Section)`
   color: ${colors.darkShade};
   background-color: ${colors.lightShade};
 `;
 
-const Card = styled.div`
-  height: 50%;
-  width: 100%;
-  border-radius: 8px;
-  padding: 12px;
-`;
-
-const PhotoWrapper = styled(Card)`
-  box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.375);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: ${colors.darkShade};
-  overflow: hidden;
-  height: fit-content;
-  max-height: 50%;
-  width: fit-content;
-  max-width: 95%;
-  padding: 16px;
-
-  ${media.landscape`
-    max-width: 45%;
-    max-height: 100%;
-    align-self: center;
-  `}
-`;
-
-const PhotoBorder = styled.div`
-  border-radius: 4px;
-  border: 1.2px solid ${colors.lightAccent};
-  box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175);
-  overflow: hidden;
-  height: 100%;
-`;
-
-const CardSection = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  ${media.landscape`
-  width: 45%;
-  flex-direction: column;
-  gap: 64px 0;
-`};
-`;
-
-const DetailsWrapper = styled(Card)`
-  box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.375);
-  background-color: ${colors.white};
-  height: fit-content;
-  padding-bottom: 24px;
-  width: 98%;
-
-  ${media.landscape`
-    border-radius: initial;
-    background-color: inherit;
-    box-shadow: unset;
-  `}
-`;
-
-const CardTitle = styled.h2`
-  margin: 38px auto 6px;
-  font-size: 2rem;
-  text-align: center;
-  width: fit-content;
-  text-transform: uppercase;
-  color: ${colors.darkShade};
-
-  ${media.landscape`
-    margin: 28px auto 42px;
-    box-shadow: inset 0px 0rem 3rem 5px rgb(0 0 0 / 38%);
-    background-color: ${colors.mainColor};
-    color: ${colors.white};
-    width:100%;
-    border-radius: 8px;
-    padding: 12px;
-
-  `}
-`;
-
-const CardAccent = styled.h3`
-  color: ${colors.mainColor};
-  font: roboto slab;
-  font-size: 0.8rem;
-`;
-
-function Projects() {
-  const data = useStaticQuery(graphql`
-    query {
-      allProjectListJson {
-        nodes {
-          id
-          name
-          description
-          img {
-            childImageSharp {
-              gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  const projectSlides = data.allProjectListJson?.nodes
-    ?.reverse()
-    .map((project) => {
-      const projectPhoto = getImage(project.img);
-
-      return (
-        <SwiperSlide key={project.id}>
-          <PhotoWrapper>
-            <PhotoBorder>
-              <GatsbyImage
-                image={projectPhoto}
-                alt={`Screenshot of ${project.name}`}
-              />
-            </PhotoBorder>
-          </PhotoWrapper>
-          <CardSection>
-            <CardTitle>{project.name}</CardTitle>
-            <DetailsWrapper>
-              <Body>{project.description}</Body>
-              <CardAccent>Front-End:</CardAccent>
-              <CardAccent>Back-End:</CardAccent>
-            </DetailsWrapper>
-          </CardSection>
-        </SwiperSlide>
-      );
-    });
-
+// Wrapper for projects layout allowing interchanging project card layout
+// depending on breakpoint definitions
+function ProjectView({ children }) {
   return (
     <ProjectsSection id="projects">
       <Parallax
-        opacity={[-2, 2]}
+        opacity={[-1, 1]}
+        translateY={[-100, 0]}
         easing="easeInOutQuad"
+        speed={-5}
+        shouldAlwaysCompleteAnimation
       >
         <Title>Projects</Title>
       </Parallax>
+      {children}
+    </ProjectsSection>
+  );
+}
+
+function Projects() {
+  // laptop and tablet breakpoints interpolated into REM
+  const laptopBreakpointStr = useMemo(
+    () => `(min-width:${laptopBreakpoint}rem)`,
+    [],
+  );
+
+  const tabletBreakpointStr = useMemo(
+    () =>
+      `(min-width:${mobileBreakpoint}rem) and (max-width:${laptopBreakpoint}rem)`,
+    [],
+  );
+
+  // array with boolean values if in tablet or laptop view, otherwise assume we
+  // are on mobile
+  const breakpoint = {
+    isTablet: useMediaQuery(tabletBreakpointStr),
+    isLaptop: useMediaQuery(laptopBreakpointStr),
+  };
+
+  if (breakpoint.isLaptop) {
+    const data = useProjectSlide(breakpoint);
+
+    return (
+      <ProjectView>
+        <ProjectLaptopView data={data} />
+      </ProjectView>
+    );
+  }
+
+  const projectSlides = useProjectSlide(breakpoint);
+
+  return (
+    <ProjectView>
       <Swiper
-        modules={[Pagination, Autoplay, Mousewheel]}
+        modules={[Pagination, Autoplay, Mousewheel, EffectCube]}
         pagination={{ type: 'bullets' }}
-        autoplay={{ delay: 5000 }}
-        mousewheel={{ forceToAxis: true }}
-        breakpoints={{
-          320: { spaceBetween: 32 },
-          600: { spaceBetween: 64 },
-        }}
+        effect={'cube'}
+        cubeEffect={{ shadow: false }}
+        autoplay={{ delay: 5000, disableOnInteraction: true }}
+        mousewheel={{ forceToAxis: true, releaseOnEdges: true }}
       >
         {projectSlides}
       </Swiper>
-    </ProjectsSection>
+    </ProjectView>
   );
 }
 
---
+++ src/utils/useProjectSlides.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { SwiperSlide } from 'swiper/react';
+import ProjectSlide from '../components/ProjectSlide';
+
+function useProjectSlide(breakpoint) {
+  // this is a Gatsby hook using graphQL to fetch JSON data and use saved images correctly
+  const data = useStaticQuery(graphql`
+    query {
+      allProjectListJson {
+        nodes {
+          id
+          name
+          description
+          img {
+            childImageSharp {
+              gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (breakpoint.isLaptop) return data;
+
+  // creates an array of JSX article elements representing each Project seperately from
+  // JSON data parsed by useStaticQuery above in reverse order
+  const projectElementArr = data.allProjectListJson?.nodes
+    ?.reverse()
+    .map((project) => (
+      <ProjectSlide
+        project={project}
+        key={project.id}
+      />
+    ));
+
+  // utility function wrapping two project elements in SwiperSlide component
+  const combineTwoProjectElements = (Arr) =>
+    Arr.map((group) => (
+      <SwiperSlide key={`double${group[0].key}`}>
+        {group[0]}
+        {group[1]}
+      </SwiperSlide>
+    ));
+
+  // utility function wrapping each project element in SwiperSlide component
+  const onePerSlide = (projectArr) =>
+    projectArr.map((projEl) => (
+      <SwiperSlide key={`single${projEl.key}`}>{projEl}</SwiperSlide>
+    ));
+
+  // utility function creates 2D array, each internal array consists of two Project elements
+  const twoPerSlide = (projectArr) => {
+    const projectSlidesArr = [[]];
+    let count = 0;
+
+    projectArr.forEach((element) => {
+      if (projectSlidesArr[count]?.length === 0) {
+        projectSlidesArr[count] = [element];
+      } else if (projectSlidesArr[count]?.length === 1) {
+        projectSlidesArr[count].push(element);
+      } else if (projectSlidesArr[count]?.length === 2) {
+        count += 1;
+        projectSlidesArr[count] = [element];
+      }
+    });
+
+    return combineTwoProjectElements(projectSlidesArr);
+  };
+
+  if (breakpoint.isTablet) {
+    return twoPerSlide(projectElementArr);
+  }
+
+  return onePerSlide(projectElementArr);
+}
+
+export default useProjectSlide;
