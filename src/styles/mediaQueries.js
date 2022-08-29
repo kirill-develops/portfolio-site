59a271afd9f4a755a7cd975744852d7ad76fbb3f
Kirill Tchentsov <kirill.develops@gmail.com>
2022-08-28 23:00:09 -0400

    correct syntax error regarding breakpointStr var

--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -1,15 +1,16 @@
 import { css } from 'styled-components';
 
 export const mobileBreakpoint = 481 / 16;
-export const tabletBreakpoint = 601 / 16;
+export const tabletMinBreakpoint = 601 / 16;
 export const laptopBreakpoint = 1025 / 16;
+export const tabletMaxBreakpoint = 820 / 16;
 export const desktopBreakpoint = 1081 / 16;
 
 // laptop and tablet breakpoints interpolated into REM
 export const mobileLandscapeBreakpointStr = `(max-width:${laptopBreakpoint}rem) and (max-height: ${mobileBreakpoint}rem) and (orientation: landscape)`;
 export const tabletPortraitBreakpointStr = `(min-width:${mobileBreakpoint}rem) and (max-width:${laptopBreakpoint}rem) and (orientation: portrait)`;
-export const tabletLandscapeBreakpointStr = `(min-width:${mobileBreakpoint}rem) and (max-width:${laptopBreakpoint}rem) and (min-height${mobileBreakpoint}rem) and (max-height:${820 / 16}rem) and (orientation: landscape)`;
-export const laptopBreakpointStr = `(min-width:${laptopBreakpoint + 1}rem)`;
+export const tabletLandscapeBreakpointStr = `(min-height:${mobileBreakpoint}rem) and (max-height:${tabletMaxBreakpoint}rem) and (orientation: landscape)`;
+export const laptopBreakpointStr = `(min-width:${laptopBreakpoint}rem)`;
 
 const media = {
   landscape: (...args) => css`
@@ -25,11 +26,11 @@ const media = {
       ${css(...args)}
     }`,
   tabletLandscape: (...args) => css`
-    @media (min-width: ${tabletBreakpoint + 1}em) and (max-width: ${laptopBreakpoint}em) and (min-height:${mobileBreakpoint + 1}) and (orientation: landscape) {
+    @media (min-width: ${tabletMinBreakpoint + 1}em) and (max-width: ${laptopBreakpoint}em) and (min-height:${mobileBreakpoint + 1}) and (orientation: landscape) {
       ${css(...args)}
     }`,
   tabletPortrait: (...args) => css`
-    @media (min-width: ${tabletBreakpoint}em) and (max-width: ${laptopBreakpoint}em) and (orientation: portrait) {
+    @media (min-width: ${tabletMinBreakpoint}em) and (max-width: ${laptopBreakpoint}em) and (orientation: portrait) {
       ${css(...args)}
     }`,
   mobileLandscape: (...args) => css`
