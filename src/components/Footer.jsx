13143aaf99d484ddcf41edfcdd675bd9db193fc2
Kirill Tchentsov <kirill.develops@gmail.com>
2022-09-12 14:16:23 -0400

    updated background-color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,13 @@ const pulseAnimation = keyframes`
 `;
 
 const Contact = styled(Section)`
-  background-color: ${colors.black};
+  background: linear-gradient(to bottom, #323232 0%, #3f3f3f 40%, #1c1c1c 150%),
+    linear-gradient(
+      to top,
+      rgba(255, 255, 255, 0.4) 0%,
+      rgba(0, 0, 0, 0.25) 200%
+    );
+  background-blend-mode: multiply;
   position: relative;
   height: unset;
   padding-bottom: 12px;
